fc6482d5e2ad82658496f9da14fd0fe0cbd17167
孔朋 <kongpeng@daixiaomi.com>
2023-06-26 14:29:37 +0800

    build: add deploy

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "kp",
   description: "kp's home",
-  base: "/blogs/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
